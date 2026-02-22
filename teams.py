e87fad5e773cae8640df6f4e662d4c594a225d50
SPADO <padojinog.sf@obmontessori.edu.ph>
2026-02-22 19:48:49 +0800

    Update teams.py

--- teams.py
+++ teams.py
@@ -13,7 +13,7 @@ def check_eligibility(e):
     if not grade_input or not section_input or not registered_input or not medical_input:
         display("❌ Please fill all fields.", target="result")
 
-    grades = int(grade_input)
+    grade = int(grade_input)
     registered = registered_input.value
     medical = medical_input.value
 
@@ -21,7 +21,7 @@ def check_eligibility(e):
         display("❌ You must register online.", target="result")
     if medical != "Yes":
         display("❌ You need medical clearance.", target="result")
-    if grades < 7 or grades > 10:
+    if grade < 7 or grade > 10:
         display("❌ Only Grades 7-10 are eligible.", target="result")
 
     teams = {
@@ -35,4 +35,5 @@ def check_eligibility(e):
         display(f"🎉 Congratulations! You are Eligible! You're Team {teams[section_input]}!", target="result")
     else:
         display("❌ Invalid section.", target="result")
+
 
