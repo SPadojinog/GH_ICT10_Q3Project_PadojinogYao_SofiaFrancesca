1f018f84902c8f86a9ab4116601bd38c6d4f3a00
SPADO <padojinog.sf@obmontessori.edu.ph>
2026-02-23 12:48:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,33 +3,33 @@ from pyscript import display, document
 
 
 def sign_up_account(e):
-     # clears previous answers
     document.getElementById("output").innerHTML = "" 
     username = document.getElementById("username").value.strip() 
     password = document.getElementById("password").value
 
     # INFORMATION
     if username == "" and password == "": 
-        display("❌ Please fill in the information before creating account.", target="output") #Displays if no input in username and password
+        display("❌ Please fill in the information before creating account.", target="output") 
     elif username == "": 
-        display("❌ Please input your username.", target="output") #Displays if no input in username
+        display("❌ Please input your username.", target="output")
     elif password == "":
-        display("❌ Please input your password.", target="output")  #Displays if no input in password
+        display("❌ Please input your password.", target="output")
 
     # CHARACTERS 
     elif len(username) < 7: 
-        display("❌ Username must contain at least 7 characters.", target="output") #Displays if username lower than or equal to 7 characters
+        display("❌ Username must contain at least 7 characters.", target="output") 
     elif len(password) < 10:
-        display("❌ Password must contain at least 10 characters.", target="output") #Displays if password lower than or equal 10 characters
+        display("❌ Password must contain at least 10 characters.", target="output") 
 
     # PASSWORD
     elif password.isalpha(): 
-        display("❌ Password must contain at least one number.", target="output") #Displays if password is only all letters
+        display("❌ Password must contain at least one number.", target="output")
     elif password.isdigit(): 
-        display("❌ Password must contain at least one letter.", target="output") #Displays if password is only all numbers
+        display("❌ Password must contain at least one letter.", target="output") 
     
     else:
         display("✅ Account successfully created!", target="output")
+
 
 
 
