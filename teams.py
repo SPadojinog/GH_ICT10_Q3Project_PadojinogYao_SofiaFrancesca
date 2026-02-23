b64378efdbf1ba32266f1a2e8bf383387575781c
SPADO <padojinog.sf@obmontessori.edu.ph>
2026-02-23 12:24:36 +0800

    Update teams.py

--- teams.py
+++ teams.py
@@ -1,40 +1,38 @@
 # Team Checker
 from pyscript import document, display
 
+
 def check_eligibility(e):
-    registered_input = document.querySelector('input[name="registered"]:checked')
-    medical_input = document.querySelector('input[name="medical"]:checked')
-    grade_input = document.getElementById("grade").value
-    section_input = document.getElementById("section").value.lower()
+    registered_input = document.querySelector('input[name="registered"]:checked') #
+    medical_input = document.querySelector('input[name="medical"]:checked') #
+    grade_input = document.getElementById("grade").value #
+    section_input = document.getElementById("section").value.lower() #
 
-    document.getElementById("result").innerHTML = ""
+    document.getElementById("result").innerHTML = "" #clears previous answers
 
-    # Check if fields are empty
-    if not grade_input or not section_input or not registered_input or not medical_input:
-        display(f"❌ Please fill all fields.", target="result")
+    if not grade_input or not section_input or not registered_input or not medical_input:     # Check if fields are empty
+        display(f"❌ Please fill all fields.", target="result") #
 
     else:
-        grade = int(grade_input)
-        registered = registered_input.value
+        grade = int(grade_input) 
+        registered = registered_input.value 
         medical = medical_input.value
 
-        if registered != "Yes":
-            display(f"❌ You must register online.", target="result")
-        elif medical != "Yes":
+        if registered != "Yes": #Displays if they have not registered
+            display(f"❌ You must register online.", target="result") 
+        elif medical != "Yes":  #Displays if they have no medical clearance
             display(f"❌ You need medical clearance.", target="result")
-        elif grade < 7 or grade > 10:
+        elif grade < 7 or grade > 10:  #Displays that only grade 7-10 are allowed
             display(f"❌ Only Grades 7-10 are eligible.", target="result")
         else:
-            teams = {
+            teams = {  #Displays if they input emerald in the section field, they are in Blue Bears
                 "emerald": "Blue Bears 🐻",
                 "ruby": "Yellow Tigers 🐯",
                 "sapphire": "Red Bulldogs 🐶",
                 "topaz": "Green Hornets 🐝"
             }
 
-            if section_input in teams:
-                display(f"🎉 Congratulations! You are Eligible! You're Team {teams[section_input]}!", target="result")
-            else:
-                display(f"❌ Invalid section.", target="result")
-
-
+            if section_input in teams:  #Displays all fields are filled and correct
+                display(f"🎉 Congratulations! You are Eligible! You're Team {teams[section_input]}!", target="result") 
+            else: 
+                display(f"❌ Invalid section.", target="result")  #Displays if incorrect section given
