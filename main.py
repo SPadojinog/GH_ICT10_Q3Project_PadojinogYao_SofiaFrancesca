44dbfde29c90d9765ff9899e85fac37a96a5479c
SPADO <padojinog.sf@obmontessori.edu.ph>
2026-02-23 12:40:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,25 +8,26 @@ def sign_up_account(e):
     password = document.getElementById("password").value
 
     # INFORMATION
-    if username == "" and password == "": #Displays if no input in username and password
-        display("❌ Please fill in the information before creating account.", target="output")
-    elif username == "": #Displays if no input in username
-        display("❌ Please input your username.", target="output")
-    elif password == "": #Displays if no input in password
-        display("❌ Please input your password.", target="output")
+    if username == "" and password == "": 
+        display("❌ Please fill in the information before creating account.", target="output") #Displays if no input in username and password
+    elif username == "": 
+        display("❌ Please input your username.", target="output") #Displays if no input in username
+    elif password == "":
+        display("❌ Please input your password.", target="output")  #Displays if no input in password
 
     # CHARACTERS 
-    elif len(username) =< 7: #Displays if username lower than or equal to 7 characters
-        display("❌ Username must contain at least 7 characters.", target="output")
-    elif len(password) =< 10: #Displays if password lower than or equal 10 characters
-        display("❌ Password must contain at least 10 characters.", target="output")
+    elif len(username) < 7: 
+        display("❌ Username must contain at least 7 characters.", target="output") #Displays if username lower than or equal to 7 characters
+    elif len(password) < 10:
+        display("❌ Password must contain at least 10 characters.", target="output") #Displays if password lower than or equal 10 characters
 
     # PASSWORD
-    elif password.isalpha(): #Displays if password is only all letters
-        display("❌ Password must contain at least one number.", target="output")
-    elif password.isdigit(): #Displays if password is only all numbers
-        display("❌ Password must contain at least one letter.", target="output")
+    elif password.isalpha(): 
+        display("❌ Password must contain at least one number.", target="output") #Displays if password is only all letters
+    elif password.isdigit(): 
+        display("❌ Password must contain at least one letter.", target="output") #Displays if password is only all numbers
     
     else:
         display("✅ Account successfully created!", target="output")
+
 
