dcd2fdb21a9858d8b878c3eea9ff4b460a8c817d
SPADO <padojinog.sf@obmontessori.edu.ph>
2026-02-23 12:41:50 +0800

    Update players.py

--- players.py
+++ players.py
@@ -3,16 +3,18 @@ from pyscript import display, document
 
 
 def player_list (e):
-    document.getElementById('output').innerHTML = " " # clears the output / prevent's code from repeating
-
-    players = [ # names of students in 10 Sapphire
+    # clears the output / prevent's code from repeating
+    document.getElementById('output').innerHTML = " "
+     # names of students in 10 Sapphire
+    players = [ 
         "1) Aseo", "2) Batac", "3) Calanglang", "4) Dee", "5) De Guzman",
         "6) Dolor", "7) Galvez", "8) Garces", "9) Grospe", "10) Hizon",
         "11) Intalan", "12) Ko", "13) Lagman", "14) Law", "15) Macabago",
         "16) Martinez", "17) Medina", "18) Mendoza", "19) Mergal", "20) Ngo",
         "21) Padojinog", "22) Rivera", "23) Shrestha", "24) Uy", "25) Yao"
     ]
-
-    for player in players: # displays the list when the 'output' button is clicked
+    # displays the list when the 'output' button is clicked
+    for player in players: 
         display(player, target='output')
+
 
